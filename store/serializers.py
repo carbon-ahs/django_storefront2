a44e605651bbd13c19a193f891eb9025dd71fd37
carbon-ahs <shehanuk.ahsan@gmail.com>
2023-12-27 18:01:19 +0600

    27 push

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,51 @@
+from decimal import Decimal
 from rest_framework import serializers
 
+from store.models import Product, Collection
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = [
+            "id",
+            "title",
+            "products_count",
+        ]
+
+    products_count = serializers.IntegerField()
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "title",
+            "description",
+            "slug",
+            "inventory",
+            "unit_price",
+            "collection",
+            "price_with_tax",
+        ]
+
+    price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
+
+
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(
+#         max_digits=6,
+#         decimal_places=2,
+#         source="unit_price",
+#     )
+#     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+#     collection = CollectionSerializer()
+
+#     def calculate_tax(self, product: Product):
+#         return product.unit_price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -1,19 +1,74 @@
 from django.shortcuts import get_object_or_404, get_list_or_404
+from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
+from rest_framework import status
+from .models import Collection, Product
+from .serializers import CollectionSerializer, ProductSerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def product_list(request):
-    queryset = get_list_or_404(Product)
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        queryset = get_list_or_404(Product)
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # print(serializer.validated_data)
+        # if serializer.is_valid():
+        #     return Response("ok")
+        # else:
+        #     return Response(
+        #         serializer.errors,
+        #         status=status.HTTP_400_BAD_REQUEST,
+        #     )
 
 
-@api_view()
+@api_view(["GET", "PUT", "DELETE"])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["GET", "POST"])
+def collection_list(request):
+    if request.method == "GET":
+        queryset = Collection.objects.annotate(products_count=Count("product"))
+        serializer = CollectionSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def collection_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    if request.method == "GET":
+        serializer = CollectionSerializer(product)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CollectionSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
