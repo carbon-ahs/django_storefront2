b5ab6ef5e12b24415b07ad2586cd1c98c1ddb607
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-01-01 12:41:35 +0600

    Custom Filter Added

--- store/views.py
+++ store/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import get_object_or_404
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
+from store.filters import ProductFilter
 from .models import Collection, OrderItem, Product, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get("collection_id")
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         context = {
@@ -31,6 +29,32 @@ class ProductViewSet(ModelViewSet):
         return super().destroy(request, *args, **kwargs)
 
 
+class ProductViewSetOld(ModelViewSet):
+    #     serializer_class = ProductSerializer
+
+    #     def get_queryset(self):
+    #         queryset = Product.objects.all()
+    #         collection_id = self.request.query_params.get("collection_id")
+    #         if collection_id is not None:
+    #             queryset = queryset.filter(collection_id=collection_id)
+    #         return queryset
+
+    #     def get_serializer_context(self):
+    #         context = {
+    #             "request": self.request,
+    #         }
+    #         return context
+
+    #     def destroy(self, request, *args, **kwargs):
+    #         if OrderItem.objects.filter(product_id=kwargs["pk"]).count() > 0:
+    #             error_message = {
+    #                 "error": "Product can't be deleted.",
+    #             }
+    #             return Response(error_message)
+    #         return super().destroy(request, *args, **kwargs)
+    pass
+
+
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count("products")).all()
     serializer_class = CollectionSerializer
@@ -41,21 +65,13 @@ class CollectionViewSet(ModelViewSet):
         }
         return context
 
-    # def destroy(self, request, *args, **kwargs):
-    #     if OrderItem.objects.filter(product_id=kwargs["pk"]).count() > 0:
-    #         error_message = {
-    #             "error": "Collection can't be deleted. ",
-    #         }
-    #         return Response(error_message)
-    #     return super().destroy(request, *args, **kwargs)
-
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs["pk"]).count() > 0:
             error_message = {
-                "error": "Collection can't be deleted.",
+                "error": "Collection can't be deleted. ",
             }
             return Response(error_message)
+        return super().destroy(request, *args, **kwargs)
 
 
 class ReviewViewSet(ModelViewSet):
